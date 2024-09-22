7b8eede53db6db1b0259e7718adfadaaac4a8e07
chimezdev <ochuba6@gmail.com>
2024-09-22 11:40:44 +0100

    signup screen msg

--- app/src/main/java/com/example/shirtersdroid/ui/onboarding/SignUpScreen.kt
+++ app/src/main/java/com/example/shirtersdroid/ui/onboarding/SignUpScreen.kt
@@ -1,6 +1,5 @@
 package com.example.shirtersdroid.ui.onboarding
 
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
